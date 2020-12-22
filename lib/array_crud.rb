3a4084cda185c2dadde87a7ed450c61da27f974a
Joshua Morales <joshuapmorales@gmail.com>
2020-12-22 21:10:20 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -31,7 +31,7 @@ end
 def remove_element_from_end_of_array(array)
   
   remove_element_from_end_of_array = ["array"]
-  "arrays!" = remove_element_from_end_of_array.pop
+  array = remove_element_from_end_of_array.pop
   
 end
 
