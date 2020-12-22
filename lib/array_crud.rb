00efc460ba4dfafebc36b1db92e0f783b562c7e4
Joshua Morales <joshuapmorales@gmail.com>
2020-12-22 21:05:34 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,9 +19,13 @@ def add_element_to_end_of_array(array, element)
   
 end
 
+
 def add_element_to_start_of_array(array, element)
   
+  add_element_to_start_of_array = ["array", "element"]
+  add_element_to_start_of_array.unshift()
 end
+
 
 def remove_element_from_end_of_array(array)
   
